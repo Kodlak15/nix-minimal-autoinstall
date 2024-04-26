e0c91924e658698251bd876c0af2617b9f53709a
Kodlak15 <stanlcod15@gmail.com>
2024-04-26 12:16:46 -0700

    Experimenting with further functionality

--- autoinstall.sh
+++ autoinstall.sh
@@ -39,12 +39,12 @@ configuration="
     ./hardware-configuration.nix
   ];
 
-  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.settings.experimental-features = [\"nix-command\" \"flakes\"];
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "default";
+  networking.hostName = \"default\";
   networking.networkmanager.enable = true;
 
   sound.enable = true;
@@ -52,7 +52,7 @@ configuration="
 
 	users.users.$username = {
     isNormalUser = true;
-    extraGroups = ["wheel"];
+    extraGroups = [\"wheel\"];
     packages = with pkgs; [
       gnumake
       git
@@ -73,7 +73,7 @@ configuration="
   services.openssh.enable = true;
 
   # Do not change this value!
-  system.stateVersion = "$version";
+  system.stateVersion = \"$version\";
 }
 "
 
