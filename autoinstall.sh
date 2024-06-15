bdf34afe585176903c3848418df35914ba5f0b36
Kodlak15 <stanlcod15@gmail.com>
2024-06-14 22:48:26 -0700

    Got basic FDE working

--- autoinstall.sh
+++ autoinstall.sh
@@ -9,32 +9,38 @@ if [[ ! "$EUID" -eq 0 ]]; then
 	exit 1
 fi
 
+# Disk to be used and its partitions
+DISK="/dev/vda"
+BOOTPART="/dev/vda1"
+ROOTPART="/dev/vda2"
+
+# The size of the boot partition (mb)
+BOOTSIZE=512
+
+# Mountpoint used during installation
+MOUNTPOINT="/mnt/nixos"
+
+# Slot to be used on the yubikey (set to 0 to skip programming)
+SLOT="0"
+
 # Program the yubikey
-# ykpersonalize -2 -ochal-resp -ochal-hmac
-
-# The name of the virtual disk to install on
-disk="/dev/vda"
-
-# The names of the partitions
-boot="/dev/vda1"
-root="/dev/vda2"
-
-# The mount point used for the installation
-mountpoint="/mnt/nixos"
+if [[ ! "$SLOT" == "0" ]]; then
+	ykpersonalize -"$SLOT" -ochal-resp -ochal-hmac
+fi
 
 # Create the partitions
-sgdisk -g "$disk"
-sgdisk -n 1::+256M --typecode=1:ef00 "$disk"
-sgdisk -n 2::-0 --typecode=2:8300 "$disk"
-partprobe "$disk"
-
-# Calculate LUKS passphrase
-SALT_LENGTH=16
-SALT="$(dd if=/dev/random bs=1 count=$SALT_LENGTH 2>/dev/null | rbtohex)"
+sgdisk -g "$DISK"
+sgdisk -n 1::+"$BOOTSIZE"M --typecode=1:ef00 "$DISK"
+sgdisk -n 2::-0 --typecode=2:8300 "$DISK"
+partprobe "$DISK"
 
 # Read 2FA password
 echo "Enter 2FA password:"
 read -s USER_PASSPHRASE
+
+# Calculate the initial salt (length can be any integer in range 16-64)
+SALT_LENGTH=16
+SALT="$(dd if=/dev/random bs=1 count=$SALT_LENGTH 2>/dev/null | rbtohex)"
 
 # Calculate the initial challenge and response
 CHALLENGE="$(echo -n $SALT | openssl dgst -binary -sha512 | rbtohex)"
@@ -48,65 +54,59 @@ LUKS_KEY="$(echo -n $USER_PASSPHRASE | pbkdf2-sha512 $(($KEY_LENGTH / 8)) $ITERA
 # Create the LUKS device
 CIPHER=aes-xts-plain64
 HASH=sha512
-echo -n "$LUKS_KEY" | hextorb | cryptsetup luksFormat --label "NIXOS" --cipher="$CIPHER" --key-size="$KEY_LENGTH" --hash="$HASH" --key-file=- "$root"
+echo -n "$LUKS_KEY" | hextorb | cryptsetup luksFormat --label "NIXOS" --cipher="$CIPHER" --key-size="$KEY_LENGTH" --hash="$HASH" --key-file=- "$ROOTPART"
 
 # Create the boot filesystem
-mkfs.fat -F 32 -n "EFI-NIXOS" "$boot"
+mkfs.fat -F 32 -n "EFI-NIXOS" "$BOOTPART"
 
 # Store the salt and iterations on the boot volume
-mount --mkdir "$boot" /boot
+mount --mkdir "$BOOTPART" /boot
 mkdir -p /boot/crypt-storage
 echo -ne "$SALT\n$ITERATIONS" > /boot/crypt-storage/default
 umount /boot
 
 # Open the LUKS device
-echo -n "$LUKS_KEY" | hextorb | cryptsetup open "$root" nixos-crypt --key-file=-
+echo -n "$LUKS_KEY" | hextorb | cryptsetup open "$ROOTPART" nixos-crypt --key-file=-
 
-# TODO this is less than ideal
-#
-# Reassign root to /dev/mapper/nixos-crypt
-root="/dev/mapper/nixos-crypt"
+# The encrypted root partition of the new system
+ROOT="/dev/mapper/nixos-crypt"
 
-# Create root filesytem 
-mkfs.btrfs -L "NIXOS" "$root" -f
+# Create the root filesytem 
+mkfs.btrfs -L "NIXOS" "$ROOT" -f
 
 # Create subvolumes
-mount --mkdir "$root" "$mountpoint"
-btrfs subvolume create "$mountpoint/@"
-btrfs subvolume create "$mountpoint/@home"
-btrfs subvolume create "$mountpoint/@tmp"
-btrfs subvolume create "$mountpoint/@var"
-umount "$mountpoint"
+mount --mkdir "$ROOT" "$MOUNTPOINT"
+btrfs subvolume create "$MOUNTPOINT/@"
+btrfs subvolume create "$MOUNTPOINT/@home"
+btrfs subvolume create "$MOUNTPOINT/@tmp"
+btrfs subvolume create "$MOUNTPOINT/@var"
+umount "$MOUNTPOINT"
 
 # Mount the subvolumes
-mount -o subvol="@" "$root" "$mountpoint"
-mount --mkdir -o subvol="@home" "$root" "$mountpoint/home"
-mount --mkdir -o subvol="@tmp" "$root" "$mountpoint/tmp"
-mount --mkdir -o subvol="@var" "$root" "$mountpoint/var"
+mount -o subvol="@" "$ROOT" "$MOUNTPOINT"
+mount --mkdir -o subvol="@home" "$ROOT" "$MOUNTPOINT/home"
+mount --mkdir -o subvol="@tmp" "$ROOT" "$MOUNTPOINT/tmp"
+mount --mkdir -o subvol="@var" "$ROOT" "$MOUNTPOINT/var"
 
 # Mount the boot partition
-mount --mkdir "$boot" "$mountpoint/boot"
+mount -o umask=0077 --mkdir "$BOOTPART" "$MOUNTPOINT/boot"
 
 # Generate the initial configuration
-nixos-generate-config --root "$mountpoint"
+nixos-generate-config --root "$MOUNTPOINT"
 
 # TODO there is probably a better way to handle this
 #
 # Replace the initial configuration with the minimal configuration
-# url="https://raw.githubusercontent.com/Kodlak15/nix-minimal-autoinstall/master/configuration.nix"
-# config="$(curl "$url")"
-config="$(cat ./configuration.nix)"
-echo "$config" > "$mountpoint/etc/nixos/configuration.nix"
+config="$(curl https://raw.githubusercontent.com/Kodlak15/nix-minimal-autoinstall/master/configuration.nix)"
+echo "$config" > "$MOUNTPOINT/etc/nixos/configuration.nix"
 
 # Install the system
-nixos-install --root "$mountpoint"
+nixos-install --root "$MOUNTPOINT"
 
 # Unmount all volumes
-umount -R "$mountpoint"
+umount -R "$MOUNTPOINT"
 
 # Close the LUKS partition
 cryptsetup luksClose "nixos-crypt"
 
 echo "NixOS was installed successfully!"
-echo "You may now reboot and begin building your system."
-echo "If you are going to build using a flake, be sure to add the generated hardware-configuration.nix to the repository."
